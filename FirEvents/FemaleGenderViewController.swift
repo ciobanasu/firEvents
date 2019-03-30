6e9ce9da9f037220b18afbbaa5209b53402ff19c
Johnny <nikusor.94@gmail.com>
2019-03-30 22:38:55 +0200

    A firebase event

---
+++ FirEvents/FemaleGenderViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FemaleGenderViewController.swift
+//  FirEvents
+//
+//  Created by Ciobanasu Ion on 3/30/19.
+//  Copyright © 2019 Ciobanasu Ion. All rights reserved.
+//
+
+import UIKit
+
+class FemaleGenderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FirEvents/MaleGenderViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MaleGenderViewController.swift
+//  FirEvents
+//
+//  Created by Ciobanasu Ion on 3/30/19.
+//  Copyright © 2019 Ciobanasu Ion. All rights reserved.
+//
+
+import UIKit
+
+class MaleGenderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
